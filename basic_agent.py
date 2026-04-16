5763a2748623bbf41821103b769c91fdfcece9f5
LakshmiReddy <klakshmireddy49@gmail.com>
2026-04-16 22:39:35 +0530

    Delete basic_agent.py

--- basic_agent.py
+++
@@ -1,9 +0,0 @@
-import ollama
-response = ollama.chat(
-    model="phi3:mini",
-    messages=[
-        {"role": "user", "content": "Explain artificial intelligence"}
-    ]
-)
-
-print(response["message"]["content"])
